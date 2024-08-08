c006d46df943dffe63c063d3a1ff03e3e02e4a60
Mark Antony <45166294+MarkAntonyGit@users.noreply.github.com>
2024-08-08 15:14:00 +0530

    Modified script

--- cowsay.sh
+++ cowsay.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get install cowsay -y
-cowsay -f dragon "Hi All" >> dragon.txt
+cowsay -f dragon "Hi All" >> dragon.txt1
 cowsay "GitHub Actions is easy to learn"
 for f in /usr/share/cowsay/cows/*; do cowsay -f "${f:23:-4}" "${f:23:-4}"; done
-cat dragon.txt
+cat dragon.txt1
